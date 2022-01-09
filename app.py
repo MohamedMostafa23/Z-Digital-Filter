9bcc7bee9e7e9f1baf86b809067d4b4fefe642c3
MohamedMostafa23 <61363974+MohamedMostafa23@users.noreply.github.com>
2022-01-09 20:12:10 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, jsonify
+import pandas as pd
+from scipy import signal as sig
+import pandas as pd
+import requests
+import io
+
+
+import math 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+	
+# @app.route('/upload', methods = ['GET', 'POST'])
+# def upload_file():
+#     if request.method == 'POST':
+
+#     return render_template('index.html')
+        
+@app.route('/plot', methods = ['GET', 'POST'])
+def digital_filter_data():
+    url = "https://raw.githubusercontent.com/MohamedMostafa23/Digital-Filter/main/Data-Nor-ECG.csv"
+    data_url = requests.get(url).content
+    Xaxis_data = pd.read_csv(io.StringIO(data_url.decode('utf-8'))).iloc[:, 0].tolist()
+    Yaxis_data = pd.read_csv(io.StringIO(data_url.decode('utf-8'))).iloc[:, 1].tolist()
+    Filtered_data = []
+    zeros = []
+    poles = []
+
+    if request.method == 'POST':
+        data = request.json
+        for index in range(len(data)):
+            if data[index]['type'] == "nonConjZero" or data[index]['type'] == "ConjZero":
+                zero = complex(data[index]['point'][0], data[index]['point'][1])
+                zeros.append(zero)
+
+            if data[index]['type'] == "nonConjPole" or data[index]['type'] == "ConjPole":
+                pole = complex(data[index]['point'][0], data[index]['point'][1])
+                poles.append(pole)
+
+
+    sysDnum, sysDden = sig.zpk2tf(zeros, poles, 1)
+    filtered_data_withComplex = sig.lfilter(sysDnum, sysDden, Yaxis_data)
+    for index in range(len(filtered_data_withComplex)):
+        absolute = abs(filtered_data_withComplex[index])
+        Filtered_data.append(absolute)
+
+    data = jsonify({"data_Xaxis": Xaxis_data, "non_Filtered_Data" : Yaxis_data, "Filtered_Data" : Filtered_data})
+
+    return data
+
+if __name__ == '__main__':
+    
+    app.run(debug=True)
